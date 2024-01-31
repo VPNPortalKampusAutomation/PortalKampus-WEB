1686cfed64bc85c0033503f81a314e8366cad97b
knmukaffa <knmukafa@gmail.com>
2024-01-31 10:54:24 +0700

    Initial Commit

---
+++ Config/dataconfig.py
@@ -0,0 +1,29 @@
+class TestData:
+
+    """this is base of url"""
+    BASE_URL_STAGING = ""
+    BASE_URL_UAT = ""
+    BASE_URL_PROD = ""
+
+    # """this is all test data needed for verify/assert"""
+
+    """this is all credential account"""
+    # General Password :
+    VALID_PASSWORD = "password123*"
+    INVALID_PASSWORD = "invalid123"
+    BLANK_PASSWORD = ""
+    # STUDENT EMAIL & USERNAME
+    STUDENT_EMAIL = ""
+    STUDENT_USERNAME = ""
+    # LECTURER EMAIL & USERNAME
+    LECTURER_EMAIL = ""
+    LECTURER_USERNAME = ""
+    # ADMIN MAJOR EMAIL & USERNAME
+    ADMIN_MAJOR_EMAIL = ""
+    ADMIN_MAJOR_USERNAME = ""
+    # ADMIN FACULTY EMAIL & USERNAME
+    ADMIN_FACULTY_EMAIL = ""
+    ADMIN_FACULTY_USERNAME = ""
+    # ADMIN UNIVERSITY EMAIL & USERNAME
+    ADMIN_UNIVERSITY_EMAIL = ""
+    ADMIN_UNIVERSITY_USERNAME = ""
