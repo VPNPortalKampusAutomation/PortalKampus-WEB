cd3e277530d396e542d9c2242ab5e6590d58fd79
Miftah Ramadhan VPN <miftah.ramadhan@noble-software.com>
2024-02-08 22:53:47 +0700

    loc_dashboard_page.py

---
+++ Pages/loc_dashboard_page.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+class LocatorMainContent:
+    # LOC_TEACHER_WELCOME_TITLE = (By.XPATH, '//div[@class="pageHeading"]/p/em')
+    # LOC_MODAL_INPUT_EMAIL = (By.XPATH, '//div[@class="modal-content email-change-modal"]')
+    pass
+
+class LocatorSidebar:
+    pass
+
+class LocatorNavbar:
+    pass
+
+class LocatorFooter:
+    pass
