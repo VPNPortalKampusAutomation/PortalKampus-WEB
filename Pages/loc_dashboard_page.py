e422e1c814c0be1884a6dd89073e1ed032ad7ce9
Miftah Ramadhan VPN <miftah.ramadhan@noble-software.com>
2024-02-15 21:31:14 +0700

    add locator footer in dashboard & update basemethod

--- Pages/loc_dashboard_page.py
+++ Pages/loc_dashboard_page.py
@@ -12,4 +12,7 @@ class LocatorNavbar:
     pass
 
 class LocatorFooter:
+    LOC_COPYRIGHT_TXT = (By.XPATH, '//div[@class="MuiStack-root joy-13011bp"]/span')
+    LOC_TERMOFUSE_TXT = (By.XPATH, "//a[text()='Terms Of Use' or text()='Syarat Ketentuan Penggunaan']")
+    LOC_PRIVACYPOLCY_TXT = (By.XPATH, "//a[text()='Kebijakan Privasi' or text()='Privacy Policy']")
     pass
