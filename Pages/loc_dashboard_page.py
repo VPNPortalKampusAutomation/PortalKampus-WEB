df419e6033b1d4c52603f20a3ddb1b520ffd982a
Miftah Ramadhan VPN <miftah.ramadhan@noble-software.com>
2024-02-15 22:50:57 +0700

    add step_dashboard_page.py

--- Pages/loc_dashboard_page.py
+++ Pages/loc_dashboard_page.py
@@ -1,18 +1,28 @@
 from selenium.webdriver.common.by import By
 
-class LocatorMainContent:
-    # LOC_TEACHER_WELCOME_TITLE = (By.XPATH, '//div[@class="pageHeading"]/p/em')
-    # LOC_MODAL_INPUT_EMAIL = (By.XPATH, '//div[@class="modal-content email-change-modal"]')
+
+class LocatorNavbar:
+    pass
+
+class LocatorMainContentAdminUniversity:
+    pass
+
+class LocatorMainContentAdminFaculty:
+    pass
+
+class LocatorMainContentAdminMajor:
+    pass
+
+class LocatorMainContentLecturer:
+    pass
+
+class LocatorMainContentStudent:
     pass
 
 class LocatorSidebar:
     pass
 
-class LocatorNavbar:
-    pass
-
 class LocatorFooter:
     LOC_COPYRIGHT_TXT = (By.XPATH, '//div[@class="MuiStack-root joy-13011bp"]/span')
-    LOC_TERMOFUSE_TXT = (By.XPATH, "//a[text()='Terms Of Use' or text()='Syarat Ketentuan Penggunaan']")
+    LOC_TERMOFUSE_TXT = (By.XPATH, "//a[text()='Terms of Use' or text()='Syarat Ketentuan Penggunaan']")
     LOC_PRIVACYPOLCY_TXT = (By.XPATH, "//a[text()='Kebijakan Privasi' or text()='Privacy Policy']")
-    pass
