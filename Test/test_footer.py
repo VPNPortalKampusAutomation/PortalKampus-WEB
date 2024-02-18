d0c85e1d743b5abf7bad9b586689a714895c2de8
Miftah Ramadhan VPN <miftah.ramadhan@noble-software.com>
2024-02-18 21:33:03 +0700

    update test_footer.py

--- Test/test_footer.py
+++ Test/test_footer.py
@@ -5,31 +5,34 @@ from StepsDefinition.step_navbar import StepDefNavbarAllRole
 @pytest.mark.usefixtures("setup_scope_function")
 class TestFooter:
 
-    def __init__(self):
-        self.footer = StepDefFooter(self.driver)
-        self.navbar = StepDefNavbarAllRole(self.driver)
-
     def test_verify_theres_copyright(self):
+        footer = StepDefFooter(self.driver)
         # this row will be place the step for login user
-        self.footer.check_copyright_is_visible()
+        footer.check_copyright_is_visible()
 
     def test_verify_copyright_text_is_correct(self):
+        footer = StepDefFooter(self.driver)
         # this row will be place the step for login user
-        self.footer.check_copyright_text_is_correct()
+        footer.check_copyright_text_is_correct()
 
     def test_verify_theres_termsofuse(self):
+        footer = StepDefFooter(self.driver)
         # this row will be place the step for login user
-        self.footer.check_terms_of_use_is_visible()
+        footer.check_terms_of_use_is_visible()
 
     def test_verify_termsofuse_text_indo_is_correct(self):
+        footer = StepDefFooter(self.driver)
+        navbar = StepDefNavbarAllRole(self.driver)
         # this row will be place the step for login user
-        self.navbar.change_language_to_indonesia()
-        self.footer.check_terms_of_use_text_indo()
+        navbar.change_language_to_indonesia()
+        footer.check_terms_of_use_text_indo()
 
     def test_verify_termsofuse_text_english_is_correct(self):
+        footer = StepDefFooter(self.driver)
+        navbar = StepDefNavbarAllRole(self.driver)
         # this row will be place the step for login user
-        self.navbar.change_language_to_english()
-        self.footer.check_terms_of_use_text_english()
+        navbar.change_language_to_english()
+        footer.check_terms_of_use_text_english()
 
 
 
