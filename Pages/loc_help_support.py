b33fa001a815cad7c8559818372d7f70065be73d
aisvpn <aisah.solehah@noble-software.com>
2024-02-28 16:34:23 +0700

    add locator profile setting

--- Pages/loc_help_support.py
+++ Pages/loc_help_support.py
@@ -2,9 +2,10 @@
 from selenium.webdriver.common.by import By
 
 class LocatorCategory:
-    LOC_TITTLE_CATEGORY = (By.XPATH, '//p[@class="MuiTypography-root MuiTypography-title-lg joy-1hipf4c"]')
-    LOC_ICON_CATEGORY = (By.XPATH, '//div[@class="HelpCenter_boxIcon__86dv2"]')
-    LOC_CARD_CATEGORY = (By.XPATH, '//button[@class="MuiButtonBase-root MuiCardActionArea-root joy-fmtgi3"]')
+    LOC_TITTLE_CATEGORY = (By.XPATH, '(//p[normalize-space()="Profile"])[1]')
+    LOC_ICON_CATEGORY = (By.XPATH, '(//div[@class="HelpCenter_boxIcon__86dv2"])[2]')
+    LOC_CARD_CATEGORY = (By.XPATH, '(//button[@class="MuiButtonBase-root MuiCardActionArea-root joy-fmtgi3"])[1]')
+    LOC_SEARCH_BAR = (By.XPATH, '(//input[@placeholder="Search help center" or @placeholder="Cari pusat bantuan"])[1]')
 
 class LocatorFAQ:
     LOC_TOPIC_LIST = (By.XPATH, '//p[@class="MuiTypography-root MuiTypography-title-sm joy-1goy8p5"]')
--- Pages/loc_profile_setting.py
+++ Pages/loc_profile_setting.py
@@ -9,5 +9,12 @@ class LocatorSettingProfile:
     LOC_SELECT_NATIONALITY = (By.XPATH, '//button[@id=":r2s:"]')
     LOC_NIK_FIELD = (By.XPATH, 'id=":r2u:"')
     LOC_SELECT_RELIGION = (By.XPATH, '//button[@id=":r2v:"]')
+    LOC_DROPDOWN_ISLAM = (By.XPATH, '//li[@id=":r1g:"]')
+    LOC_DROPDOWN_PROTESTAN = (By.XPATH, 'id=":r1h:"')
+    LOC_DROPDOWN_KATOLIK = (By.XPATH, '//li[@id=":r1i:"]')
+    LOC_DROPDOWN_BUDHA = (By.XPATH, '//li[@id=":r1j:"]')
+    LOC_DROPDOWN_HINDU = (By.XPATH, '//li[@id=":r1k:"]')
+    LOC_DROPDOWN_BLOODTYPE = (By.XPATH, '//div[@class="MuiSelect-root Mui-expanded MuiSelect-variantOutlined MuiSelect-colorNeutral MuiSelect-sizeMd joy-fkpdb6"]')
+    LOC_BLOODTYPE_APLUS = (By.XPATH, '//li[@id=":r1l:"]')
     LOC_MOTHER_NAME = (By.XPATH, '//input[@id=":rk:"]')
 
